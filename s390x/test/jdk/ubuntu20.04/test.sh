ca8c7615624a017e3783991d82cff4999957f35d
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-10-21 08:59:55 -0400

    Intentionally Introduce error

--- s390x/test/jdk/ubuntu20.04/test.sh
+++ s390x/test/jdk/ubuntu20.04/test.sh
@@ -8,7 +8,7 @@ echo -e "$ANSI_YELLOW \n*** FUNCTIONAL TEST(S) ***\n $ANSI_RESET"
 
 echo -e "$ANSI_YELLOW It can run a Java program: $ANSI_RESET"
 docker build -t test/run-app/quay.io/ibmz/openjdk runs-java-programs
-docker run --rm --name runs-java-programs test/run-app/quay.io/ibmz/openjdk
+docker run -it --rm --name runs-java-programs test/run-app/quay.io/ibmz/openjdk
 docker rmi test/run-app/quay.io/ibmz/openjdk
 
 echo -e "$ANSI_GREEN \n*** FUNCTIONAL TEST(S) COMPLETED SUCESSFULLY ***\n $ANSI_RESET"
