6e8ccd5f22990eaa45cc0cd85280e75b72ef54e0
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-10-21 09:15:28 -0400

    fix ANSI colors

--- s390x/test/jdk/ubuntu20.04/runs-java-programs/Main.java
+++ s390x/test/jdk/ubuntu20.04/runs-java-programs/Main.java
@@ -4,7 +4,7 @@ public class Main
         public static final String ANSI_RESET = "\u001B[0m";
 
         public static void main (String[] args) {
-                System.out.println(ANSI_GREEN + "Success! This Java program is running on the quay.io/ibmz/openjdk image!");
-                System.out.println("Test Complete. Terminating program..." + ANSI_RESET);
+                System.out.println(ANSI_GREEN + "Success! This Java program is running on the quay.io/ibmz/openjdk image!" + ANSI_RESET);
+                System.out.println(ANSI_GREEN + "Test Complete. Terminating program..." + ANSI_RESET);
         }
 }
