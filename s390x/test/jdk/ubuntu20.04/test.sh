1b18692e6c55074588326155b4e95be7b913fa08
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-10-21 09:16:56 -0400

    Formatting

--- s390x/test/jdk/ubuntu20.04/test.sh
+++ s390x/test/jdk/ubuntu20.04/test.sh
@@ -6,11 +6,11 @@ export ANSI_YELLOW="\e[1;33m"
 export ANSI_GREEN="\e[32m"
 export ANSI_RESET="\e[0m"
 
-echo -e "$ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET"
+echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW It can run a Java program: $ANSI_RESET"
 docker build -t test/run-app/quay.io/ibmz/openjdk runs-java-programs
 docker run --rm --name runs-java-programs test/run-app/quay.io/ibmz/openjdk
 docker rmi test/run-app/quay.io/ibmz/openjdk
 
-echo -e "$ANSI_GREEN *** FUNCTIONAL TEST(S) COMPLETED SUCESSFULLY *** $ANSI_RESET"
+echo -e "\n $ANSI_GREEN *** FUNCTIONAL TEST(S) COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
